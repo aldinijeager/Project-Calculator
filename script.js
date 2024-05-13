11877f9b471b039c08207ecc51497afdf4c42b8f
aldinijeager <aldinijeager@gmail.com>
2024-05-13 15:03:10 +0800

    create operate function in script.js file

--- script.js
+++ script.js
@@ -14,3 +14,21 @@ function divide(a, b){
 let num1,
     num2,
     operator;
+
+function operate(num1, num2, operator){
+    
+    switch (operator){
+        case "+":
+            return add(num1, num2);
+            break;
+        case "-":
+            return subtract(num1, num2);
+            break;
+        case "*":
+            return multiply(num1, num2);
+            break;
+    case "/":
+            return divide(num1, num2);
+            break;
+    }
+}
